a7df2e9f7e73b51ee05dd4c28522850755194142
Mateus <teus.alves.sp@gmail.com>
2023-10-14 12:40:47 -0300

    Guia Press - NodeJs

--- database/database.js
+++ database/database.js
@@ -1,9 +1,8 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiapress','victordev','123456abc',{
-    host: 'mysql669.umbler.com',
+const connection = new Sequelize('guiapress','userdb','passworddb',{
+    host: 'localhost',
     dialect: 'mysql',
-    timezone: "-03:00"
 });
 
 module.exports = connection;
